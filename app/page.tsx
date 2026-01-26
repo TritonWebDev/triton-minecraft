d7523fc31b571fc770eb68206a4f4df15eaa2dec
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2026-01-26 15:57:37 -0800

    Updated homepage to have a video background and fixed splashtext

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,40 @@ export default function Home() {
   return (
     <main>
       
-      <Image
-          src={background}
-          alt="Background Image"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          priority
-          className="relative z-[-1] blur-[2px]"
-      />
+      {/* Video background */}
+      <div className="absolute inset-0 w-full h-full overflow-hidden">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="absolute w-full h-full object-cover scale-110" 
+        >
+          <source 
+            src="/videos/minecraftbgvideo.mp4" 
+            type="video/mp4" 
+          />
+          <p>Your browser does not support the video tag.</p>
+        </video>
+        
+        {/* Dark overlay for better text visibility */}
+        <div className="absolute inset-0 bg-black/40"></div>
+      </div>
 
       <Image
           src={logo}
           alt="Logo"
           width={500}
           height={500}
-          className="absolute top-1/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          className="absolute top-1/6 left-1/2 transform -translate-x-1/2 -translate-y-44"
       />
 
-      {/* Splash Text TODO: POSITION CORRECTLY */}
-      <SplashText className="absolute bottom-1/2 left-1/2" text="Triton Minecraft" />
-      <Navbar />
+      {/* Splash Text*/}
+      <SplashText className="absolute bottom-1/2 left-3/5 transform -translate-x-1/6 translate-y-65 rotate-[-20deg] relative z-50 text-2xl" text="Triton Minecraft" />
+
+      <div className="relative z-48 absolute bottom-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/8">
+        <Navbar />
+      </div>
 
       <p className="absolute bottom-0 left-0 text-white">
         &copy; {new Date().getFullYear()} Triton Minecraft
